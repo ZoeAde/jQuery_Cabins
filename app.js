37f980619647e0c3305f4fb0ecd3e8feca8cedb2
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-30 10:08:39 -0600

    completed high and low button functionality

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
 
-
+//New Homes
   $('.image-container').append('<article class="home" data-price="1000000000"><img src="http://www.lehmannmansion.com/uploads/O2/aR/O2aRtzGaHZgBzexcG2uTMg/Leh_Front_Exterior-1.jpg" /><h4>$1 MILLION</h4></article>');
 
   $('.image-container').append('<article class="home" data-price="50000"><img src="http://www.lehmannmansion.com/uploads/Xa/Q_/XaQ_TOycqXjLg8qlOVF32w/Lehmann_Mansion_web.jpg" /><h4>$50,000</h4></article>');
 
   $('.image-container').append('<article class="home" data-price="30000"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Rundown_Shack.jpg/1280px-Rundown_Shack.jpg" /><h4>$30,000</h4></article>');
-
+//Changing Light to Dark
   $('img').css({'height': '150px', 'width': '150px'});
 
   $('.light').on('click', function(){
@@ -18,6 +18,7 @@ $(document).ready(function() {
     $('body').css('background-image', 'url(img/dark_wood.png)');
   });
 
+//Forest Background
   $('.textures').append('<span class="forest"></span>');
 
   $('.forest').on('click', function(){
@@ -28,52 +29,66 @@ $(document).ready(function() {
 
   $('.textures').css("width", "150px");
 
-  // $('.low').on("click", function(){
-
-  // });
-
+//Low to High Sort
 var homeArray = $('.home').slice();
-var homeLow = [];
-homeArray.sort(function(a, b){
+var lowBtn = $('.low');
+var homePlacement = $('.image-container');
+var lowHigh = homeArray.sort(lowToHigh);
 
-var aHome = parseInt(a.getAttribute("data-price"));
-console.log(aHome);
-var bHome = parseInt(b.getAttribute("data-price"));
-console.log(bHome);
+function lowToHigh(a, b) {
+  var aHome = parseInt(a.dataset.price);
+  var bHome = parseInt(b.dataset.price);
 
-  if (aHome < bHome) {
-    console.log("round1");
-    return a;
-  }
-  else if (bHome < aHome){
-    console.log("round2");
-    return b;
-  }
-  else {
-    console.log("round3");
-    return a, b;
-  }
+    if (aHome > bHome) {
+      return 1;
+    }
+    else if (bHome > aHome) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+lowBtn.on("click", function(){
+  homePlacement.html(lowHigh);
 });
 
+//High to Low Sort
+var homeArray2 = $('.home').slice();
+var highBtn = $('.high');
+var highLow = homeArray2.sort(highToLow);
 
-// function sort(array) {
-//   var newArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     if (parseInt(array[i].dataset.price) < parseInt(array[i + 1].dataset.price)) {
-//       console.log(parseInt(array[i + 1].dataset.price));
-//       newArray.push(array[i]);
-//     }
-//     else {
-//       console.log(parseInt(array[i].dataset.price));
-//       newArray.push(array[i+1]);
-//     }
-//   }
-//   return newArray;
-// }
+function highToLow(a,b) {
+  var aHome = parseInt(a.dataset.price);
+  var bHome = parseInt(b.dataset.price);
+
+    if (aHome < bHome) {
+      return 1;
+    }
+    else if (bHome < aHome) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+highBtn.on("click", function(){
+  homePlacement.html(highLow);
+});
+
+//
 
 
 
 
 
 
+
+
+
+
+
+});
 
